bbeaa15895d137d41c3a7b66b2dedaee62dfb069
Travis Jungroth <jungroth@gmail.com>
2017-01-10 14:28:32 -0800

    Update parse with doctests and set up to doctest autorun (#8)

--- americano/__init__.py
+++ americano/__init__.py
@@ -5,45 +5,18 @@ from numbers import Number
 from operator import ge, gt, le, lt
 import re
 
-# This module implements a parser and evaluator for a restricted subset of JavaScript.
-#
-# It is implemented using a variation of a Crockford TDOP style Pratt parser modified to
-# support creation of an evaluable node tree on parse.
-#
-# See these links for algorithm details:
-#   - http://javascript.crockford.com/tdop/tdop.html
-#   - http://eli.thegreenplace.net/2010/01/02/top-down-operator-precedence-parsing/
 
-
-def parse(expression, global_context={}):
+def parse(expression, global_context=None):
     """
     Takes a string and returns a parsed expression, which can be evaluated with a context.
 
-    The language accepted is a restricted subset of javascript.  The input must be an expression, not a statement.
-    The following operators are supported:
-        ternary operator (?:)
-        logical operators (||, &&)
-        comparison operators (===, ==, !===, !=, <. <=, >, >=)
-        addition/subtraction (+, -)
-        multiplication/division (*, /)
-        unary logical not (!)
-        unary negation/plus (-, +)
-        function invocation('(')
-        computed member access ('[')
-    In addition, the following syntax constructs are supported:
-        array literal
-        null
-        true
-        false
-        single quoted string
-        double quoted string
-        floating point literal (no exponential notation)
-        integer literal
-     Example:
-         parsed = parse('var == 2')
-         parsed.eval({'var': 1})
+    >>> p = parse('var == 4')
+    >>> p.eval({'var': 4})
+    True
+    >>> p.eval({'var': 5})
+    False
     """
-    return Parser(expression).root(global_context)
+    return Parser(expression).root(global_context or {})
 
 
 class ParseError(Exception):
