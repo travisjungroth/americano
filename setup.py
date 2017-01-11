95cc89bff07f64e20de337780df82c25711c5f53
Travis Jungroth <jungroth@gmail.com>
2017-01-10 18:09:31 -0800

    Update setup and readme (#15)

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 from setuptools import setup
 
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
 setup(
     name='americano',
     version='0.0.0',
     packages=['americano'],
     license='MIT',
     install_requires=['six'],
+    long_description=readme(),
+    author_email='jungroth@gmail.com',
+    author='Travis Jungroth',
+    url='http://github.com/travisjungroth/americano',
 )
