3f0ea52a512c95731d6f42b07835286e9cc29fd3
Travis Jungroth <tjungroth@housecanary.com>
2017-01-10 19:04:50 -0800

    Change readme to markdown, add python versions to setup

--- setup.py
+++ setup.py
@@ -3,11 +3,15 @@ from setuptools import setup
 
 setup(
     name='americano',
-    version='0.0.3',
+    version='0.0.4',
     packages=['americano'],
     license='MIT',
     install_requires=['six'],
     author_email='jungroth@gmail.com',
     author='Travis Jungroth',
     url='http://github.com/travisjungroth/americano',
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+    ],
 )
