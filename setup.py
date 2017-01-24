65e7fc69794031c173a7c2facd85c7681dee47b7
Travis Jungroth <tjungroth@housecanary.com>
2017-01-24 09:37:32 -0800

    Add undefined

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='americano',
-    version='0.0.5',
+    version='0.1.0',
     packages=['americano'],
     license='MIT',
     install_requires=['six'],
